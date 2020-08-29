571436a2b6284307581da40269feea4a66287b18
Sam Jovanou <samjovanou@gmail.com>
2020-08-29 11:52:40 +0800

    Sparta

--- pages/sparta.tsx
+++ pages/sparta.tsx
@@ -21,7 +21,7 @@ export const SpartaSection: FC = () => (
                 <p>Sparta’s Initial Distribution is via Proof-of-Burn. Participants burn their previous assets in order to acquire a share of 100,000,000 $SPARTA. </p>
                 <p>The remaining 200,000,000 $SPARTA is emitted to participants for the provision of liquidity in liquidity pools via a supply-responsive emission schedule. </p>
                 <p>A protocol fee-burn ensures 300,000,000 is never reached, and eventually fees will offset emissions. </p>
-                <p>The token distribution phase has not yet commenced, to know exactly when launch is hapening; make sure you are subscribed:</p>
+                <p>The token distribution phase has not yet commenced, to know exactly when launch is happening; make sure you are subscribed:</p>
                 <a href="https://medium.com/@spartanprotocol" target="_blank">
                   <p>Spartan Protocol's Medium</p>
                 </a>
