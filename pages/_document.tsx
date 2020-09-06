06bbb3ad9900a84895c1cc6254430a967adc01df
Sam J <samjovanou@gmail.com>
2020-09-06 08:17:12 +0800

    Meta tags update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -34,19 +34,15 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="Spartan Protocol" />
+          <meta property="og:title" content="Spartan Protocol | DeFi on BSC" />
           <meta property="og:image" content="https://spartanprotocol.org/spartan-preview.png" />
-          <meta property="og:description" content="A protocol for incentivised liquidity and synthetic tokens" />
+          <meta property="og:description" content="A protocol for incentivised liquidity & synthetic tokens. Swap BEP assets & earn yield!" />
           <meta property="og:url" content="https://spartanprotocol.org" />
           <meta name="twitter:card" content="https://spartanprotocol.org/spartan-preview.png" />
-          <meta
-            name="keywords"
-            content="Liquidity, Swap Assets"
-          />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no"
-          />
+          <meta name="title" content="Spartan Protocol | DeFi on BSC" />
+          <meta name="description" content="A protocol for incentivised liquidity & synthetic tokens. Swap BEP assets & earn yield!" />
+          <meta name="keywords" content="Liquidity, Swap Assets, BSC, DeFi" />
+          <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no" />
           <meta name="google-site-verification" content="lMyRmPCukXIx7zODSb7jqgrMw-67IjkVHZNbkNGfgXs" />
           <link
             href="https://fonts.googleapis.com/css?family=Roboto:300,400|Open+Sans:200,300,400|Titan+One"
