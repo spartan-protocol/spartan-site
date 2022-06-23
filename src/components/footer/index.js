6e8343a67de68cc055781856944ad2778ccc7f6c
Jejo <jejopl@o2.pl>
2022-06-23 11:24:38 +0200

    attempt to fix ssr errors

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,7 +2,7 @@ import * as React from "react";
 
 const Footer = () => {
     // TODO: get real updated at
-    const lastUpdated = Date.now()
+    const lastUpdated = "2022"
     return (
         <div className="text-white relative z-10 flex justify-center">
             <div className="mb-2">Last updated at {lastUpdated}</div>
