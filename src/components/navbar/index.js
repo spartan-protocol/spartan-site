665c0b5356c3b2f928014f26699ecf624d2fc0b4
Jejo <jejopl@o2.pl>
2022-07-29 10:46:20 +0200

    Added font loading check only for dapp button It's because this is the most noticeable element, when the font is not loaded. It's using css 'scale' attribute and with default font it looks really bad.

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -41,54 +41,60 @@ const Navbar = () => {
     }, 2000);
   });
 
-  return (
-    (isFontLoaded || fontLoadedTimeout) && (
-      <div className="flex bg-black w-full p-2 px-4 justify-between fixed z-20 animate-[fadeIn_0.3s_ease_forwards]">
-        <div className="flex align-center w-36">
-          <a href="#home">
-            <SpartaIcon width="30px" height="30px" />
-          </a>
-        </div>
-        <div className="hidden sm:flex text-white items-center space-x-6 text-lg text-center">
-          <a className="hover:opacity-60 transition" href="#home">
-            Home
-          </a>
-          <a className="hover:opacity-60 transition" href="#swap">
-            Swap
-          </a>
-          <a className="hover:opacity-60 transition" href="#pool">
-            Pool
-          </a>
-          <a className="hover:opacity-60 transition" href="#stake">
-            Stake
-          </a>
-          <a className="hover:opacity-60 transition" href="#dao">
-            DAO
-          </a>
-          <a className="hover:opacity-60 transition" href="#team">
-            Team
-          </a>
-          <a className="hover:opacity-60 transition" href="#token">
-            Token
-          </a>
-        </div>
-        <div className="flex items-center justify-end w-36 pr-2">
-          <div className="mr-4 sm:hidden block">
-            <div className="p-2" onClick={() => toggleNav()}>
-              <MenuIcon className="cursor-pointer transition hover:opacity-60" height="20px" fill="white" />
+  const dappButton = () => {
+    return (
+      (isFontLoaded || fontLoadedTimeout) && (
+        <div className="animate-[fadeIn_0.3s_ease_forwards]">
+          <a href="https://dapp.spartanprotocol.org">
+            <div className={`text-white border text-xs p-1 font-sairaCondensed ${isFontLoaded && "scale-x-125"}`}>
+              <div className="px-1.5">OPEN DAPP</div>
             </div>
-            <MobileMenu closeNav={closeNav} />
-          </div>
-          <div>
-            <a href="https://dapp.spartanprotocol.org">
-              <div className={`text-white border text-xs p-1 font-sairaCondensed ${isFontLoaded && "scale-x-125"}`}>
-                <div className="px-1.5">OPEN DAPP</div>
-              </div>
-            </a>
-          </div>
+          </a>
         </div>
+      )
+    );
+  };
+
+  return (
+    <div className="flex bg-black w-full p-2 px-4 justify-between fixed z-20">
+      <div className="flex align-center w-36">
+        <a href="#home">
+          <SpartaIcon width="30px" height="30px" />
+        </a>
       </div>
-    )
+      <div className="hidden sm:flex text-white items-center space-x-6 text-lg text-center">
+        <a className="hover:opacity-60 transition" href="#home">
+          Home
+        </a>
+        <a className="hover:opacity-60 transition" href="#swap">
+          Swap
+        </a>
+        <a className="hover:opacity-60 transition" href="#pool">
+          Pool
+        </a>
+        <a className="hover:opacity-60 transition" href="#stake">
+          Stake
+        </a>
+        <a className="hover:opacity-60 transition" href="#dao">
+          DAO
+        </a>
+        <a className="hover:opacity-60 transition" href="#team">
+          Team
+        </a>
+        <a className="hover:opacity-60 transition" href="#token">
+          Token
+        </a>
+      </div>
+      <div className="flex items-center justify-end w-36 pr-2">
+        <div className="mr-4 sm:hidden block">
+          <div className="p-2" onClick={() => toggleNav()}>
+            <MenuIcon className="cursor-pointer transition hover:opacity-60" height="20px" fill="white" />
+          </div>
+          <MobileMenu closeNav={closeNav} />
+        </div>
+        {dappButton()}
+      </div>
+    </div>
   );
 };
 
