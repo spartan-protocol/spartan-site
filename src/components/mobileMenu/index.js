10dc38064051340082d5e6c0acd948f69c35c6e8
Jejo <jejopl@o2.pl>
2022-12-03 21:48:37 +0100

    attempt to fix broken nav on mobile sometimes

--- src/components/mobileMenu/index.js
+++ src/components/mobileMenu/index.js
@@ -6,28 +6,28 @@ const MobileMenu = ({ closeNav }) => {
       <div className="flex-1 flex flex-col items-center justify-center text-3xl text-gray-200">
         <div className="absolute w-full h-full cursor-default" role="button" aria-hidden="true" onClick={() => setTimeout(() => closeNav(), 50)}></div>
         <div className="relative z-30 flex flex-col space-y-8 text-center p-8 select-none">
-          <a className="transition hover:opacity-60" href="#home" onClick={() => setTimeout(() => closeNav(), 50)}>
+          <a className="transition hover:opacity-60" target="framename" href="#home" onClick={() => setTimeout(() => closeNav(), 50)}>
             Home
           </a>
-          <a className="transition hover:opacity-60" href="#swap" onClick={() => setTimeout(() => closeNav(), 50)}>
+          <a className="transition hover:opacity-60" target="framename" href="#swap" onClick={() => setTimeout(() => closeNav(), 50)}>
             Swap
           </a>
-          <a className="transition hover:opacity-60" href="#pool" onClick={() => setTimeout(() => closeNav(), 50)}>
+          <a className="transition hover:opacity-60" target="framename" href="#pool" onClick={() => setTimeout(() => closeNav(), 50)}>
             Pool
           </a>
-          <a className="transition hover:opacity-60" href="#stake" onClick={() => setTimeout(() => closeNav(), 50)}>
+          <a className="transition hover:opacity-60" target="framename" href="#stake" onClick={() => setTimeout(() => closeNav(), 50)}>
             Stake
           </a>
-          <a className="transition hover:opacity-60" href="#contributors" onClick={() => setTimeout(() => closeNav(), 50)}>
+          <a className="transition hover:opacity-60" target="framename" href="#contributors" onClick={() => setTimeout(() => closeNav(), 50)}>
             Contributors
           </a>
-          <a className="transition hover:opacity-60" href="#video" onClick={() => setTimeout(() => closeNav(), 50)}>
+          <a className="transition hover:opacity-60" target="framename" href="#video" onClick={() => setTimeout(() => closeNav(), 50)}>
             Video
           </a>
-          <a className="transition hover:opacity-60" href="#token" onClick={() => setTimeout(() => closeNav(), 50)}>
+          <a className="transition hover:opacity-60" target="framename" href="#token" onClick={() => setTimeout(() => closeNav(), 50)}>
             Tokenomics
           </a>
-          <a className="transition hover:opacity-60" href="#token" onClick={() => setTimeout(() => closeNav(), 50)}>
+          <a className="transition hover:opacity-60" target="framename" href="#token" onClick={() => setTimeout(() => closeNav(), 50)}>
             Token
           </a>
         </div>
