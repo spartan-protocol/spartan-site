12926300898adc21ebd109e7bd134efcc1f7dfef
Jejo <jejopl@o2.pl>
2023-01-11 10:30:07 +0100

    fixed landing page heigh issue on ios

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,6 @@ module.exports = {
       resolve: `gatsby-plugin-sass`,
       options: {
         postCssPlugins: [
-          require('postcss-viewport-height-correction'),
           require("tailwindcss"),
           require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
           require('autoprefixer'),
