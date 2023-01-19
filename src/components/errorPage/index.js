b3d5c3b64dca204b5104e695261964a7fe9d4101
Jejo <jejopl@o2.pl>
2023-01-19 18:04:29 +0100

    fixed 404 size on mobile

--- src/components/errorPage/index.js
+++ src/components/errorPage/index.js
@@ -5,7 +5,7 @@ const ErrorPage = () => {
     <div id="error" className="h-screen bg-black justify-center snap-start">
       <div className="flex font-saira flex-1 h-full items-center justify-center relative z-20">
         <div className="flex flex-col flex-1 justify-center items-center text-white">
-        <h1 className="text-4xl sm:text-6xl px-6 mb-2 opacity-0 animate-fadeIn tracking-[.2em] sm:tracking-[.25em] font-medium">404</h1>
+        <h1 className="text-6xl px-6 mb-2 opacity-0 animate-fadeIn tracking-[.2em] sm:tracking-[.25em] font-medium">404</h1>
         <div>Not found</div>
         </div>
       </div>
